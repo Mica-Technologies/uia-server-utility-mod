d7d497e57e3e31e8cec5561cb04c0ff39c930461
mica-alex <83238954+mica-alex@users.noreply.github.com>
2026-04-09 08:01:47 -0400

    Migrate source to com.micatechnologies.minecraft.sum package
    - Move mod from com.myname.mymodid to com.micatechnologies.minecraft.sum
    - Rename main class to Sum with SumConstants (matching CSM conventions)
    - Update mcmod.info with SUM description and Mica Technologies credits
    - Update pack.mcmeta with ServerUtilityMod name

--- src/main/java/com/micatechnologies/minecraft/sum/Sum.java
+++ src/main/java/com/micatechnologies/minecraft/sum/Sum.java
@@ -1,4 +1,4 @@
-package com.myname.mymodid;
+package com.micatechnologies.minecraft.sum;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -15,49 +15,41 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]")
-public class MyMod {
+@Mod(modid = SumConstants.MOD_NAMESPACE, version = SumConstants.MOD_VERSION, name = SumConstants.MOD_NAME, acceptedMinecraftVersions = "[1.12.2]")
+public class Sum {
 
-    public static final Logger LOGGER = LogManager.getLogger(Tags.MODID);
+    public static final Logger LOGGER = LogManager.getLogger(SumConstants.MOD_NAMESPACE);
+
+    @Mod.Instance(SumConstants.MOD_NAMESPACE)
+    public static Sum instance;
 
     @EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc. (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-        // register to the event bus so that we can listen to events
         MinecraftForge.EVENT_BUS.register(this);
-        LOGGER.info("I am " + Tags.MODNAME + " + at version " + Tags.VERSION);
+        LOGGER.info("I am " + SumConstants.MOD_NAME + " at version " + SumConstants.MOD_VERSION);
     }
 
     @SubscribeEvent
-    // Register recipes here (Remove if not needed)
     public void registerRecipes(RegistryEvent.Register<IRecipe> event) {
-
     }
 
     @SubscribeEvent
-    // Register items here (Remove if not needed)
     public void registerItems(RegistryEvent.Register<Item> event) {
-
     }
 
     @SubscribeEvent
-    // Register blocks here (Remove if not needed)
     public void registerBlocks(RegistryEvent.Register<Block> event) {
-
     }
 
     @EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
     }
 
     @EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {
     }
 
     @EventHandler
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {
     }
 }
---
+++ src/main/java/com/micatechnologies/minecraft/sum/SumConstants.java
@@ -0,0 +1,8 @@
+package com.micatechnologies.minecraft.sum;
+
+public class SumConstants {
+
+    public static final String MOD_NAMESPACE = Tags.MODID;
+    public static final String MOD_NAME = Tags.MODNAME;
+    public static final String MOD_VERSION = Tags.VERSION;
+}
