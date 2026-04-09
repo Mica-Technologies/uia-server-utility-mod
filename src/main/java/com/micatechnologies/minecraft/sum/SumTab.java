f604fc062abfd12661e70162562ab37ffa99bf42
mica-alex <83238954+mica-alex@users.noreply.github.com>
2026-04-09 08:19:56 -0400

    Fix SUM creative tab with custom Roamer spawn egg item
    Vanilla spawn eggs from .egg() are hardcoded to the Misc tab and
    can't be reassigned. Replace with a custom ItemRoamerSpawnEgg that
    is a proper registered item with setCreativeTab(SumTab.TAB).
    
    - Add ItemRoamerSpawnEgg: right-click to spawn a Roamer on any surface
    - Remove .egg() from entity registration
    - SumTab icon uses the custom spawn egg item
    - Add en_us.lang with item, entity, and tab names
    - Add item model using vanilla spawn egg textures

--- src/main/java/com/micatechnologies/minecraft/sum/SumTab.java
+++ src/main/java/com/micatechnologies/minecraft/sum/SumTab.java
@@ -2,9 +2,8 @@ package com.micatechnologies.minecraft.sum;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
-import net.minecraft.item.ItemMonsterPlacer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 
 public class SumTab {
@@ -12,20 +11,9 @@ public class SumTab {
     public static final CreativeTabs TAB = new CreativeTabs(SumConstants.MOD_NAMESPACE) {
         @Override
         public ItemStack createIcon() {
-            ItemStack egg = new ItemStack(Items.SPAWN_EGG);
-            ItemMonsterPlacer.applyEntityIdToItemStack(
-                egg, new ResourceLocation(SumConstants.MOD_NAMESPACE, "roamer"));
-            return egg;
-        }
-
-        @Override
-        public void displayAllRelevantItems(NonNullList<ItemStack> items) {
-            super.displayAllRelevantItems(items);
-            // Add the roamer spawn egg to this tab
-            ItemStack roamerEgg = new ItemStack(Items.SPAWN_EGG);
-            ItemMonsterPlacer.applyEntityIdToItemStack(
-                roamerEgg, new ResourceLocation(SumConstants.MOD_NAMESPACE, "roamer"));
-            items.add(roamerEgg);
+            Item egg = Item.REGISTRY.getObject(
+                new ResourceLocation(SumConstants.MOD_NAMESPACE, "roamer_spawn_egg"));
+            return egg != null ? new ItemStack(egg) : new ItemStack(Items.EGG);
         }
     };
 }
