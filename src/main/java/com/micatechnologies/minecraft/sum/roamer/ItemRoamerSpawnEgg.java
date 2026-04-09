97151c1c0f2a5442613af09f7fef60c207f4261e
mica-alex <83238954+mica-alex@users.noreply.github.com>
2026-04-09 08:26:56 -0400

    Add walkable block check and feedback to Roamer spawn egg
    - Check if clicked block is in the walkable blocks list before spawning
    - Show red chat message with the block name if placement is denied
    - Show error message if world.spawnEntity() fails
    - Use setLocationAndAngles for proper spawn with random facing
    - Add spawn logging

--- src/main/java/com/micatechnologies/minecraft/sum/roamer/ItemRoamerSpawnEgg.java
+++ src/main/java/com/micatechnologies/minecraft/sum/roamer/ItemRoamerSpawnEgg.java
@@ -1,7 +1,10 @@
 package com.micatechnologies.minecraft.sum.roamer;
 
+import com.micatechnologies.minecraft.sum.Sum;
+import com.micatechnologies.minecraft.sum.SumConfig;
 import com.micatechnologies.minecraft.sum.SumConstants;
 import com.micatechnologies.minecraft.sum.SumTab;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,6 +12,8 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class ItemRoamerSpawnEgg extends Item {
@@ -28,16 +33,33 @@ public class ItemRoamerSpawnEgg extends Item {
             return EnumActionResult.SUCCESS;
         }
 
-        BlockPos spawnPos = pos.offset(facing);
-        EntityRoamer roamer = new EntityRoamer(world);
-        roamer.setPosition(spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5);
-        world.spawnEntity(roamer);
-
-        ItemStack stack = player.getHeldItem(hand);
-        if (!player.capabilities.isCreativeMode) {
-            stack.shrink(1);
+        // Check if the clicked block is a walkable block for roamers
+        IBlockState clickedState = world.getBlockState(pos);
+        String registryName = clickedState.getBlock().getRegistryName().toString();
+        if (!SumConfig.isBlockWalkableByRoamer(registryName)) {
+            player.sendMessage(new TextComponentString(
+                TextFormatting.RED + "Roamers can only be placed on walkable blocks! "
+                + TextFormatting.GRAY + "(" + registryName + " is not in the walkable blocks list)"));
+            return EnumActionResult.FAIL;
         }
 
-        return EnumActionResult.SUCCESS;
+        BlockPos spawnPos = pos.offset(facing);
+        EntityRoamer roamer = new EntityRoamer(world);
+        roamer.setLocationAndAngles(
+            spawnPos.getX() + 0.5, spawnPos.getY(), spawnPos.getZ() + 0.5,
+            world.rand.nextFloat() * 360.0F, 0.0F);
+        if (world.spawnEntity(roamer)) {
+            Sum.LOGGER.info("Roamer spawned at [{}, {}, {}]",
+                spawnPos.getX(), spawnPos.getY(), spawnPos.getZ());
+            ItemStack stack = player.getHeldItem(hand);
+            if (!player.capabilities.isCreativeMode) {
+                stack.shrink(1);
+            }
+            return EnumActionResult.SUCCESS;
+        } else {
+            player.sendMessage(new TextComponentString(
+                TextFormatting.RED + "Failed to spawn Roamer at this location."));
+            return EnumActionResult.FAIL;
+        }
     }
 }
